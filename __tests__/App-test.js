42be83bca6236e88ca98920e361704858b5fd7fb
Hà Thái Hoàng <hoanght@bap.jp>
2019-09-11 13:42:46 +0700

    update packages

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -2,13 +2,11 @@
  * @format
  */
 
-import 'react-native';
-import React from 'react';
-import App from '@';
-
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import 'react-native'
+import React from 'react'
+import renderer from 'react-test-renderer'
+import App from '@'
 
 it('renders correctly', () => {
-  renderer.create(<App />);
-});
+  renderer.create(<App />)
+})
